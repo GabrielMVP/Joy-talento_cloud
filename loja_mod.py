fd41aca92371a119810d3f619763d4afb327cb56
Gabriel Moreno Cavalcanti <38257470+GabrielMVP@users.noreply.github.com>
2023-08-30 12:48:34 -0300

    Update loja_mod.py
    atualizando apenas as marcações para entendimento do código.

--- loja_mod.py
+++ loja_mod.py
@@ -1,23 +1,36 @@
-#controle de estoque
-#produtos disponíveis lista
-#codigo que leia os produtos que tenham em lista e me diga as quantidades que temos para venda.
-#substituir produtos após acabar estoque
-#--------------------------------------#
-#Sistema que mostra lista de produtos
-#possibilidade de alterar um produto por outro
-#possibilidade de adicionar mais produtos
+# Estamos modernizando  nossa loja e precisamos de um novo sistema de controle de estoque.
 
-#lista de produtos da loja:
+# ----> 1. CRIAR UM MENU PARA ACESSAR O SISTEMA DA LOJA <-----#
+
+#Geralmente anotamos todos os produtos que temos disponíveis, e quando um dos produtos acaba,
+#substituimos ele por algum outro produto.
+#Ouvi dizer que vocês podem fazer um sistema para a gente que mostra a lista com todos nossos
+#produtos, e nos deixa alterar um produto por outro.
+
+# ----> ADICIONAR A FUNÇÃO PARA SUBSTITUIR UM PRODUTO POR OUTRO DE ESCOLHA <----#
+
+# ---->  CRIAR UM ARRAY MOSTRANDO LISTA DOS ITENS QUE TEM NA LOJA DO CLIENTE. <-----#
+
+#Além disso, estamos pensando em ampliar nosso armazém, para ter mais espaço para mais produtos.
+#Então, se puderem fazer com que o sistema nos permita adicionar mais produtos à lista, e
+#qualquer outra coisa que acharem necessário, seria muito bom.
+
+# ----> CRIAR  UMA FUNÇÃO PARA ADICIONAR MAIS PRODUTOS A ARRAY <-----#
+# ----> SISTEMA PARA ADICIONAR PRODUTOS A LOJA <----#
+
+
+# ----> LISTA DE PRODUTOS BASE PARA A LOJA A ARRAY <----#
 lista_de_produtos = ['Camisa Polo', 'Bermuda', 'Short', 'Camisa Manga Longa']
 
-#Adicionando as funções para o menu
+# ----> FUNÇÃO DO MENU PARA VERIFICAR OS PRODUTOS QUE TEM NA ARRAY <----#
 def verificar_produtos():
     print('Os produtos disponíveis para venda são: ')
-#codigo para exibir função de ver produtos a seguir:
+# AÇÃO NECESSÁRIA PARA QUE O VERIFICAR_PRODUTOS POSSA PUXAR DA ARRAY A LISTA DE ITENS
+# ELE PROCURA OS PPRODUTOS QUE TEM DENTRO DA ARRAY E MOSTRA AO FUNCIONARIO QUE ACESSA A OPÇÃO
     for produto in lista_de_produtos:
         print(produto)
         
-#----Peças do estoque e quantidades----#
+# UM ESTOQUE PARA QUE NA OPÇÃÇÃO VERIFICAR A QUANTIDADE DE PRODUTOS QUE TEM NO SISTEMA.
 
 estoque = {
     'Camisa Polo': 15,
@@ -25,42 +38,54 @@ estoque = {
     'Short': 5,
     'Camisa Manga Longa': 5
 }
-#----Função verificar a quantidade de itens do estoque----#
+
+# A FUNÇÃO PARA PUXAR  DO ESTOQUE QUE VERIFICA OS ITENS E SUAS QUANTIDADES
 
 def verificar_estoque():
     print('verificar quantidade de itens: ')
     
-#codigo para exibir função de ver produtos a seguir:
+# O CODIGO INFORMA QUE PARA PEÇA, QUANTIDADE NO ESTOQUE. FUNÇÃO ITEMS()
+# PRINTAR A PEÇA DO ESTOQUE E A QUANTIDADE QUE TEM SALVA NA FUNÇÃO ESTOQUE{}
 
     for peça, quantidade in estoque.items():
         print(f'{peça}: {quantidade} unidades')
         
-#----Função substituir um produto da lista----#
+# A FUNÇÃO PARA SUBSTITUIR UM PRODUTO DO ESTOQUE POR OUTRO
 def substituir_produtos():
     print('Qual produto você quer substituir: ')
-    # solicitando ao operador para digitar qual o produto que ele quer substituir por um novo.
+    # AQUI CHAMO DE PRODUTO ANTIGO O DA LISTA E SOLICITO AO OPERADOR QUE DIGITE O NOME DO PRODUTO QUE DESEJA SUBSTITUIR
     produto_antigo = input('Digite o nome do produto que você deseja substituir:')
-#codigo para exibir função de ver produtos a seguir:
+# NO CODIGO ABAIXO EXECUTA A SUBSTITUIÇÃO DO PRODUTO SELECIONADO POR UM NOVO
+# PRECISEI PESQUISAR UM POUCO PARA CONSEGUIR FAZER FUNCIONAR ESTÁ OPÇÃO.
+# ELE PROCURA NA BASE DOS DADOS, NA LISTA DE PRODUTOS O PRODUTO ANTIGO COM O NOEM SELECIONADO
+# APÓS ENCONTRAR SOLICITA AO OPERADOR QUE DIGITE O NOME DO NOVO PRODUTO
+# ASSIM ANEXANDO A LISTA DE PRODUTOS O NOVO PRODUTO MAS DE FORMA TEMPORARIA
+# CASO O PRODUTO ANTIGO NÃO EXISTA ELE JÁ EXECUTA O SEGUNDO CODIGO COM A MENSAGEM.
 
     if produto_antigo in lista_de_produtos:
-        index = lista_de_produtos.index(produto_antigo) #Index verifica sé nesse indice do lista de produtos
+        index = lista_de_produtos.index(produto_antigo) #Index PARA VERIFICAR O PRODUTO QUE ESTÁ ANEXADO A ARRAY
         produto_novo = input('Qual o nome do novo produto a ser substituido: ')
         lista_de_produtos[index] = produto_novo
         print(f'O produto {produto_antigo} foi substituido por {produto_novo}')
     else:
         print('Produto não encontrado!')
+        
 
-#----Função adicionar um produto na lista----#
+# A FUNÇÃO PARA ADICIONAR PRODUTOS A LISTA TAMBÉM DE FORMA TEMPORARIA
 def adicionar_produto():
     print('Qual produto você quer adicionar: ')
-#codigo para exibir função de ver produtos a seguir:
-    novo_produto = input('Qual o produto que você deseja adicionar: ') #Adicionando a função para chamar o novo produto
+    
+# AQUI SOLICITO AO OPERADOR PARA QUE DIGITE O NOME DO NOVO PRODUTO.
+#  AQUI PROCURA SE O PRODUTO ESCOLHIDO JÁ EXISTE NA ARRAY CASO NÃO EXISTA ELE O ADICIONA EM SEGUIDA MOSTRA A MENSAGEM
+# CASO ELE EXISTA PULA A PRIMEIRA EXECUÇÃO E MOSTRA QUE JÁ EXISTE UM PRODUTO COM ESTE NOME NA LISTA
+    novo_produto = input('Qual o produto que você deseja adicionar: ') 
     if novo_produto not in lista_de_produtos:
         lista_de_produtos.append(novo_produto)
         print(f'O produto {novo_produto} foi adicionado à loja!')
     else:
         print('Produto já adicionado!')
 
+# CRIAÇÃO DE UM MENU DE ACESSO COM OPÇÕES
 def menu_acesso():
     print('Menu de acesso:')
     print('1. Verificar produtos disponíveis na loja: ')
@@ -68,7 +93,8 @@ def menu_acesso():
     print('3. Substituir produtos: ')
     print('4. Adicionar produto a loja: ')
     print('5. Sair')
-    
+  
+# NESSE CRIEI UM LOOP PARA QUE SE ESCOLHA UMA OPÇÃO E QUE CASO NÃO SEJÁ ESCOLHIDO NENHUM DESSES NUMEROS ELE MOSTRE QUE A OPÇÃO É INVÁLIDA.
 while True:
     menu_acesso()
     opcao = int(input('Digite a opção desejada: '))
